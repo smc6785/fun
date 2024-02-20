4feab5a1ac339db21936b494e2956618dc44cc78
smc6785smc6785 <smc6785@hotmail.com>
2024-02-19 21:59:42 -0800

    02_19_2024

---
+++ ml/nn.c
@@ -0,0 +1,8 @@
+#define NN_IMPLEMNETATION
+//https://github.com/nothings/stb/blob/master/docs/stb_howto.txt
+#include "nn.h"
+
+int main(void)
+{
+    return 0;
+}
---
+++ ml/nn.h
@@ -0,0 +1,14 @@
+#ifndef
+#define
+
+typedef stuct{
+    size_t rows;
+    size_t cols;
+    float *es;
+} mat;
+
+#endif // NN_H_
+
+#ifdef NN_IMPLEMENTATION
+
+#endif // NN_IMPLEMENTATION
