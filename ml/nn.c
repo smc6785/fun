cdd64479ecd42ca37a172337070d06f33aa5d337
smc6785smc6785 <smc6785@hotmail.com>
2024-05-05 17:36:43 -0700

    05/02/2024

--- ml/nn.c
+++ ml/nn.c
@@ -1,8 +1,10 @@
-#define NN_IMPLEMNETATION
+#define NN_IMPLEMENTATION
 //https://github.com/nothings/stb/blob/master/docs/stb_howto.txt
 #include "nn.h"
-
+//https://www.youtube.com/watch?v=L1TbWe8bVOc
 int main(void)
 {
+    Mat m = mat_alloc(2,2);
+    mat_print(m);
     return 0;
 }
