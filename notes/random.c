e3181a88db2b5617172e18c18bdffe6965706dda
smc6785smc6785 <smc6785@hotmail.com>
2024-09-21 16:43:14 -0700

    09_21_2024

--- notes/random.c
+++ notes/random.c
@@ -857,3 +857,51 @@ int searchlist(int key){
    }
    return 0;
 }
+
+#if 0
+// Define the structure of Node
+typedef struct Node {
+  
+    // Data field. Can add more data according to our need
+    int data;
+
+    // Pointer to the next node
+    struct Node *next;
+} Node;
+
+int main() {
+  
+    // Create the First Node of the Linked List
+      // This will serve as the head of the list
+    Node *first = (Node *)malloc(sizeof(Node));
+
+    // Assigning data
+    first->data = 10;
+
+    // Creating the second node in the list
+    Node *second = (Node *)malloc(sizeof(Node));
+
+    // Assigning data
+    second->data = 20;
+
+    // Creating the third node
+    Node *third = (Node *)malloc(sizeof(Node));
+
+    // Assigning data
+    third->data = 30;
+
+    // Linking the nodes
+    first->next = second; // This will create: 10 -> 20
+    second->next = third; // This will create: 10 -> 20 -> 30
+    third->next = NULL;   // This will create: 10 -> 20 -> 30 -> NULL
+
+    printf("Linked List: ");
+    Node* temp = first;
+      while(temp) {
+      printf("%d ", temp->data);
+      temp = temp->next;
+    }
+
+    return 0;
+}
+#endif
