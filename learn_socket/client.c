83864a8cb77e974c42291038e7ffffacc39600e9
smc6785smc6785 <smc6785@hotmail.com>
2025-03-07 21:32:59 -0800

    03_07_2025

--- learn_socket/client.c
+++ learn_socket/client.c
@@ -6,7 +6,9 @@
 
 #include <netinet/in.h>
 #include <unistd.h>
+#include<arpa/inet.h>
 int main(){
+  char hello[256]= "client hello world";
 
   // create a socket
   int network_socket = socket(AF_INET, SOCK_STREAM,0);
@@ -17,6 +19,7 @@ int main(){
   server_address.sin_port = htons(8080);
   server_address.sin_addr.s_addr = INADDR_ANY;
 
+  //inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
   // connection request
   int connection_status;
   connection_status = connect(network_socket,(struct sockaddi*) &server_address, sizeof(server_address));
@@ -25,11 +28,18 @@ int main(){
     printf("error\n\n");
   }
 
-  // receive the data from the server
-  char msg[256];
-  recv(network_socket, &msg,sizeof(msg),0);
+  send(network_socket, hello, 256,0);
+  printf("client hello sent \n\n");
 
-  printf("received --> %s\n",msg);
+  char buffer[256]= {0};
+  read(network_socket, buffer, 256);
+  printf("received --> %s\n",buffer);
+
+  // receive the data from the server
+  //har msg[256];
+  //recv(network_socket, &msg,sizeof(msg),0);
+
+  //printf("received --> %s\n",msg);
   close(network_socket);
   return 0;
 } 
--- learn_socket/server.c
+++ learn_socket/server.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 #include <sys/socket.h>
 #include <sys/types.h>
 
 #include <netinet/in.h>
 int main(){
-  char server_message[256]= "socket programming is";
+  char server_message[256]= "HTTP/1.1 200 OK\nContent-Type:text/plain\nContent-Length:12\n\n helloworld!";
 
   // createing the server socket
   int server_socket = socket(AF_INET, SOCK_STREAM,0);
-
   // define the server address
   struct sockaddr_in server_address;
+  int addrlen =sizeof(server_address);
+
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(8080);
   server_address.sin_addr.s_addr =INADDR_ANY;
 
   // bind the IP and port to the server
-  bind(server_socket, (struct sockadd*)&server_address, sizeof(server_address));
+  bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
 
   // liste at the port
   listen(server_socket,5);
   int client_socket;
-  client_socket = accept(server_socket, NULL, NULL);
-  // sned the meesgae on the client address
-  send(client_socket,server_message, sizeof(server_message),0);
-  
+  while(1)
+  {
+    client_socket = accept(server_socket, (struct sockaddr*)&server_address, &addrlen);
+    char buffer[256] = {0};
+    read(client_socket, buffer,256);
+    printf("server read %s\n", buffer);
+    write(client_socket, server_message, 256);
+    printf("-----------hello from server send ------\n");
+
+    // sned the meesgae on the client address
+    //send(client_socket,server_message, sizeof(server_message),0);
+  }
   return 0;
 }
 
