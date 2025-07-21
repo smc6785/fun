f97333ef83dc9b5cb1728898dab560b2daffe259
sandbox <smc6785>
2025-07-21 00:26:11 +0000

    07_20_2025

--- architecture/pci/.smc6785_pci_driver.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vagrant/fun/architecture/pci/smc6785_pci_driver.mod := { echo  /home/vagrant/fun/architecture/pci/smc6785_pci_driver.o;  echo; } > /home/vagrant/fun/architecture/pci/smc6785_pci_driver.mod
--- architecture/pci/smc6785_pci_driver.mod
+++
@@ -1,2 +0,0 @@
-/home/vagrant/fun/architecture/pci/smc6785_pci_driver.o
-
